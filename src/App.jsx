32b46dcc50e1db695305b66b67bc6f99c074763b
Yormi Altamiranda <yormian@gmail.com>
2026-06-09 07:27:43 -0500

    🔥 Commint
    commint se comento el ejerccion de la card reutilizable

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen p-8">
+    /* <div className="bg-gray-100 min-h-screen p-8">
       <h1 className="text-3xl font-bold text-center mb-4">Lista de Cards</h1>
       <div className="grid grid-cols-4 gap-6">
         <Card
@@ -29,7 +29,9 @@ function App() {
           image="https://dummyimage.com/400x300/e3e3e3/141414"
         />
       </div>
-    </div>
+    </div> */
+
+    <></>
   );
 }
 
